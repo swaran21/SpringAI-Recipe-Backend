56feeecf2e56a3d174e38dabe2294d6857672875
swaran21 <saiswaran212005@gmail.com>
2025-05-23 00:22:17 +0530

    correct path to frontend

--- src/main/java/com/ai/SpringAIProject/WebConfig.java
+++ src/main/java/com/ai/SpringAIProject/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173/")
+                        .allowedOrigins("https://firstrecipeai.netlify.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
